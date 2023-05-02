ecdf0d7753ece9bff9bf090248b1941c1cc9c353
admin <gnsruf2@gmail.com>
2023-05-02 10:40:49 +0900

    - access Token 및 refresh Token 만료시 처리

--- src/components/InfoTest.jsx
+++ src/components/InfoTest.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useMemo, useRef, useState} from 'react';
 import axios from 'axios';
 import {QueryClient, QueryClientProvider, useMutation} from "@tanstack/react-query"
 import Cookies from 'js-cookie';
@@ -11,26 +11,48 @@ function InfoTest() {
     const navigate = useNavigate();
 
     useEffect(() => {
+        fetchData();
+    }, [navigate]);
+
+    const fetchData = async () => {
         const accessToken = Cookies.get('accessToken');
-        axios.get('/auth/accessTokenAuth', {
+
+        await axios.get('/auth/accessTokenAuth', {
             headers: { Authorization: `Bearer ${accessToken}` }
         })
-            .catch(error => {
-                console.log(error);
-                // 권한이 없으면 홈 페이지로 이동
+        .catch(error => {
+            if (error.response.status === 401) {
+                const refreshToken = localStorage.getItem('refreshToken');
+
+                axios.post('/auth/reissue', {
+                    accessToken: `${accessToken}`,
+                    refreshToken: `${refreshToken}`
+                })
+                    .then(res => {
+                        const accessToken = res.data.accessToken;
+                        const refreshToken = res.data.refreshToken;
+                        Cookies.set('accessToken', accessToken);
+                        localStorage.setItem('refreshToken', refreshToken);
+                    })
+                    .catch(error => {
+                        alert("로그인 필요");
+
+                        navigate('/login');
+                    })
+            } else if (error.response.status === 403) {
+                alert("권한 부족");
+
                 navigate('/');
-            });
-        }, [navigate]);
+            }
+        });
+    };
 
     return (
         <div>
             <h1>
                 <Link to="/">홈</Link>
             </h1>
-            <br />
-            <br />
-            <br />
-            유저 권한 보유자만 볼 수 있는 페이지
+            어드민 권한 보유자만 볼 수 있는 페이지
         </div>
     );
 }
